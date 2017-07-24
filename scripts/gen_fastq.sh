54f4f2918695c83ae687502c6f3af6309974a50a
Lucas <lucas.roguski@gmail.com>
2017-07-24 18:46:12 +0200

    removed script: gen_fastq.sh --> confused with get_fastq.sh

--- scripts/gen_fastq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LINES=10000
-OUT_1=test_1.fq
-OUT_2=test_2.fq
-
-curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR174/ERR174324/ERR174324_1.fastq.gz 2>/dev/null | gunzip | head -n $LINES > $OUT_1
-curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR174/ERR174324/ERR174324_2.fastq.gz 2>/dev/null | gunzip | head -n $LINES > $OUT_2
--- scripts/get_fastq.sh
+++ scripts/get_fastq.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LINES=10000
+LINES=400000
 OUT_1=test_1.fq
 OUT_2=test_2.fq
 
-curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR024/ERR024163/ERR024163_1.fastq.gz 2>/dev/null | gunzip | head -n $LINES > $OUT_1
-curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR024/ERR024163/ERR024163_2.fastq.gz 2>/dev/null | gunzip | head -n $LINES > $OUT_2
+curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR174/ERR174324/ERR174324_1.fastq.gz 2>/dev/null | gunzip | head -n $LINES > $OUT_1
+curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR174/ERR174324/ERR174324_2.fastq.gz 2>/dev/null | gunzip | head -n $LINES > $OUT_2
